d53a7ef00b4d97f8cf76ec5e1149911b1b0275e9
CEPIZIN <lauramathias03@gmail.com>
2023-10-29 15:22:59 -0300

    home page

--- githubexplorer/src/main.tsx
+++ githubexplorer/src/main.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//PAGES
+import Home from './routes/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App /> ,
+    children:[
+      {
+        path:"/",
+        element: <Home />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
